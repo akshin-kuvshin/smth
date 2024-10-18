068110d10f1fc07242975c3b930637ce14a26fec
Danila Axyonov <akshin-kuvshin@yandex.kz>
2024-10-18 18:02:53 +0700

    Порешал Алисину задачу (найти в массиве самую длинную убывающую последовательность до минимального элемента).

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
-#define plli            pair<int, int>
+const int MAX_SIZE = (int)1e3 + 5;
+
+pair<int, int> max_decreasing_sequence(int*, int);
 
 int main() {
-    vector<char> A = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
-    for (char c1 : A) {
-        for (char c2 : A)
-            cout << c1 << c2 << ' ';
-        cout << '\n';
-    }
+    int N,
+        A[MAX_SIZE];
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+        cin >> A[i];
+    int h = (int)(min_element(A, A + N) - A);
+    pair<int, int> ans = max_decreasing_sequence(A, h);
+    cout << ans.first << ' ' << ans.second << '\n';
     return 0;
+}
+
+pair<int, int> max_decreasing_sequence(int *A, int N) {
+    int max_len = 0,
+        j = -1,
+        cur_len = 1;
+    for (int i = 1; i < N; ++i)
+        if (A[i - 1] > A[i])
+            ++cur_len;
+        else {
+            if (cur_len > max_len) {
+                max_len = cur_len;
+                j = i - cur_len;
+            }
+            cur_len = 1;
+        }
+    max_len = max(max_len, cur_len);
+    return make_pair(j, j + max_len - 1);
 }
