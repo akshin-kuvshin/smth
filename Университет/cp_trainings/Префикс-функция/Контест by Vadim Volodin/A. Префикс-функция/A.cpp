807774805a6a009978372c1248bafbbbd12601c8
Danila Axyonov <akshin-kuvshin@yandex.kz>
2025-04-12 17:58:25 +0700

    Добавил резы ЯПрофи(ПиИТ); начал изучать префикс-функцию (by Vadim Volodin).

---
+++ Университет/cp_trainings/Префикс-функция/Контест by Vadim Volodin/A. Префикс-функция/A.cpp
@@ -0,0 +1,44 @@
+// author: Danila "akshin_" Axyonov
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using lli = long long int;
+
+vector<lli> prefix(const string&);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string S;
+    getline(cin, S);
+
+    vector<lli> p = prefix(S);
+    
+    lli _sum = 0LL;
+    for (lli pi : p)
+        _sum += pi;
+    cout << _sum << '\n';
+    return 0;
+}
+
+vector<lli> prefix(const string& S) {
+    lli len = (lli)S.size();
+    vector<lli> p(len);
+    p[0] = 0LL;
+    for (int i = 1; i < (int)len; ++i) {
+        lli k = p[i - 1];
+        while (k > 0LL) {
+            if (S[i] == S[k]) {
+                p[i] = k + 1LL;
+                break;
+            }
+            k = p[k - 1LL];
+        }
+        if (S[i] == S[0])
+            p[i] = max(p[i], 1LL);
+    }
+    return p;
+}
---
+++ Университет/cp_trainings/Префикс-функция/Контест by Vadim Volodin/B. Дана строка/B.cpp
@@ -0,0 +1,55 @@
+// author: Danila "akshin_" Axyonov
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+using lli = long long int;
+
+#define pb(_elem)   push_back(_elem)
+
+vector<lli> prefix(const string&);
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    string T, S;
+    getline(cin, T);
+    getline(cin, S);
+    
+    string A = S + "#" + T;
+    vector<lli> p = prefix(A);
+    
+    lli S_len = (lli)S.size(),
+        Ap_len = (lli)p.size();
+    vector<lli> ans;
+    for (int i = 0; i < (int)Ap_len; ++i)
+        if (p[i] == S_len)
+            ans.pb((lli)i - 2LL * S_len + 1LL);
+    
+    cout << ans.size() << '\n';
+    for (lli pos : ans)
+        cout << pos << ' ';
+    cout << '\n';
+    return 0;
+}
+
+vector<lli> prefix(const string& S) {
+    lli len = (lli)S.size();
+    vector<lli> p(len);
+    p[0] = 0LL;
+    for (int i = 1; i < (int)len; ++i) {
+        lli k = p[i - 1];
+        while (k > 0LL) {
+            if (S[i] == S[k]) {
+                p[i] = k + 1LL;
+                break;
+            }
+            k = p[k - 1LL];
+        }
+        if (S[i] == S[0])
+            p[i] = max(p[i], 1LL);
+    }
+    return p;
+}
