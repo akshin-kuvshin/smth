d31e936338087edc006355c48d94d572672557e1
Danila "akshin_" Axyonov <akshin-kuvshin@yandex.kz>
2025-09-19 23:20:15 +0700

    Finished CStr class in OOP->Lab1.

--- Университет/Лабы/2025-2026/Семестр 3/ООП/1/CStr.cpp
+++ Университет/Лабы/2025-2026/Семестр 3/ООП/1/CStr.cpp
@@ -1,4 +1,7 @@
+// author: Danila "akshin_" Axyonov
+
 #include "CStr.hpp"
+#include <iostream>
 #include <cassert>
 #include <cstdlib>
 #include <cstring>
@@ -6,8 +9,11 @@
 namespace Lab1
 {
 
+using std::ostream,
+      std::istream;
 
-char* CStr::rnd_gen(int length) {
+
+char* CStr::rnd_gen(int length) const {
     const int LETTERS_NUMBER = 26;
     
     char* new_line = new char[length + 1];
@@ -19,16 +25,31 @@ char* CStr::rnd_gen(int length) {
     return new_line;
 }
 
-char* CStr::copy(const char* source) {
+inline char* CStr::copy(const char* source) const {
     int length = strlen(source);
     char* new_line = new char[length + 1];
     assert(new_line and "ERROR: couldn't allocate memory.");
-    memcpy(new_line, source, length + 1);
+    memcpy(new_line, source, length);
+    new_line[length] = '\0';
     return new_line;
 }
 
-char* CStr::copy(const CStr& object) {
+inline char* CStr::copy(const CStr& object) const {
     return this->copy(object.line);
+}
+
+inline char* CStr::concat(const CStr& object) const {
+    int length1 = this->get_length(),
+        length2 = object.get_length();
+    int result_length = length1 + length2;
+    
+    char* result_line = new char[result_length + 1];
+    assert(result_line and "ERROR: couldn't allocate memory.");
+    memcpy(result_line, this->line, length1);
+    memcpy(result_line + length1, object.line, length2);
+    result_line[result_length] = '\0';
+    
+    return result_line;
 }
 
 inline CStr::CStr() {
@@ -40,12 +61,12 @@ inline CStr::CStr() {
 }
 
 inline CStr::CStr(const char* source) {
-    assert(source and "ERROR IN [CStr::CStr(const char* source)]: source can't be a NULL-pointer.");
+    assert(source and "ERROR IN [inline CStr::CStr(const char* source)]: source can't be a NULL-pointer.");
     this->line = this->copy(source);
 }
 
 inline CStr::CStr(int length) {
-    assert(length <= 20 and "ERROR IN [CStr::CStr(int length)]: length have to be no more than 20.");
+    assert(length <= 20 and "ERROR IN [inline CStr::CStr(int length)]: length have to be no more than 20.");
     this->line = this->rnd_gen(length);
 }
 
@@ -59,7 +80,7 @@ inline CStr::~CStr() {
 }
 
 inline CStr& CStr::operator=(const char* source) {
-    assert(source and "ERROR IN [CStr& CStr::operator=(const char* source)]: source can't be a NULL-pointer.");
+    assert(source and "ERROR IN [inline CStr& CStr::operator=(const char* source)]: source can't be a NULL-pointer.");
     
     delete[] this->line;
     this->line = this->copy(source);
@@ -73,6 +94,78 @@ inline CStr& CStr::operator=(const CStr& object) {
     delete[] this->line;
     this->line = this->copy(object);
     return *this;
+}
+
+inline CStr CStr::operator+(const CStr& object) const {
+    char* result_line = this->concat(object);    
+    CStr result(result_line);
+    delete[] result_line;
+    return result;
+}
+
+inline CStr& CStr::operator+=(const CStr& object) {
+    char* result_line = this->concat(object);
+    delete[] this->line;
+    this->line = result_line;
+    return *this;
+}
+
+inline bool CStr::operator==(const CStr& object) const {
+    return strcmp(this->line, object.line) == 0;
+}
+
+inline bool CStr::operator!=(const CStr& object) const {
+    return strcmp(this->line, object.line) != 0;
+}
+
+inline bool CStr::operator<(const CStr& object) const {
+    return strcmp(this->line, object.line) < 0;
+}
+
+inline bool CStr::operator<=(const CStr& object) const {
+    return strcmp(this->line, object.line) <= 0;
+}
+
+inline bool CStr::operator>(const CStr& object) const {
+    return strcmp(this->line, object.line) > 0;
+}
+
+inline bool CStr::operator>=(const CStr& object) const {
+    return strcmp(this->line, object.line) >= 0;
+}
+
+inline char& CStr::operator[](int index) {
+    assert(0 <= index and
+           index < this->get_length() and
+           "ERROR IN [inline char& CStr::operator[](int index)]: invalid index.");
+    return this->line[index];
+}
+
+inline char CStr::operator[](int index) const {
+    assert(0 <= index and
+           index < this->get_length() and
+           "ERROR IN [inline char CStr::operator[](int index) const]: invalid index.");
+    return this->line[index];
+}
+
+inline int CStr::get_length() const {
+    return strlen(this->line);
+}
+
+ostream& operator<<(ostream& stream, const CStr& object) {
+    stream << object.line;
+    return stream;
+}
+
+istream& operator>>(istream& stream, CStr& object) {
+    const int MAX_BUFFER_SIZE = 1'000;
+    char buffer[MAX_BUFFER_SIZE];
+    stream.getline(buffer, MAX_BUFFER_SIZE, ' ');
+    
+    delete[] object.line;
+    object.line = object.copy(buffer);
+    
+    return stream;
 }
 
 
--- Университет/Лабы/2025-2026/Семестр 3/ООП/1/CStr.hpp
+++ Университет/Лабы/2025-2026/Семестр 3/ООП/1/CStr.hpp
@@ -1,17 +1,24 @@
+// author: Danila "akshin_" Axyonov
+
 #ifndef CSTR_HPP
 #define CSTR_HPP
 
+#include <iostream>
 
 namespace Lab1
 {
+
+using std::ostream,
+      std::istream;
 
 
 class CStr {
 private:
     char* line;
-    char* rnd_gen(int length);
-    char* copy(const char* source);
-    char* copy(const CStr& object);
+    char* rnd_gen(int length) const;
+    char* copy(const char* source) const;
+    char* copy(const CStr& object) const;
+    char* concat(const CStr& object) const;
 
 public:
     CStr();
@@ -21,6 +28,19 @@ public:
     ~CStr();
     CStr& operator=(const char* source);
     CStr& operator=(const CStr& object);
+    CStr operator+(const CStr& object) const;
+    CStr& operator+=(const CStr& object);
+    bool operator==(const CStr& object) const;
+    bool operator!=(const CStr& object) const;
+    bool operator<(const CStr& object) const;
+    bool operator<=(const CStr& object) const;
+    bool operator>(const CStr& object) const;
+    bool operator>=(const CStr& object) const;
+    char& operator[](int index);
+    char operator[](int index) const;
+    int get_length() const;
+    friend ostream& operator<<(ostream& stream, const CStr& object);
+    friend istream& operator>>(istream& stream, CStr& object);
 };
 
 
